f46a9359dd0852ac2b9e023d90f5e0e2e22940a3
Kimshubb <issakimanifmk@gmail.com>
2022-12-10 19:01:04 -0800

    Using the puts function in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-/*print an output on a newline. Note the puts functions returns a string that can be read using the scanf and gets function on the terminal. formatting characters return errors */
+/*main-entry point
+ * return:always 0(success) 
+*/
 int main(void){
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
