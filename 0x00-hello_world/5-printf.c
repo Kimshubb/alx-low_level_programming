d36720964aac15aabd02456ca1994631c86b4666
Kimshubb <issakimanifmk@gmail.com>
2022-12-12 07:16:15 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**prints a string 
+ *main - Entry point
+ *return 0 (success)
+*/
+
+include<stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
