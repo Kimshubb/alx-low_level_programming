3f8784de5f7b1e46be1e0de55c5a8f7022fae500
Kimshubb <issakimanifmk@gmail.com>
2022-12-12 17:34:31 -0800

    in Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - print all alphabet in lowercase in reverse
+* return 0 success
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar("alpha");
+	putchar('\n');
+	return (0);
+}
