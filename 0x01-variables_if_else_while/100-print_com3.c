d49cb31d5c3550dae3aaee50a63044676865bac9
Kimshubb <issakimanifmk@gmail.com>
2022-12-13 01:44:27 -0800

    Task 10 combinations

---
+++ 0x01-variables_if_else_while/100-print_com3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+  * main - print combinations of two digits
+  *Return :Always 0 (success)
+  */
+  int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens +'0');
+			putchar(ones +'0');
+			if (if tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
+
