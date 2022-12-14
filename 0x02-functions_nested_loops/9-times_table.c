635fc52462067dd0c5a71af781c5facecc2fe9bf
Kimshubb <issakimanifmk@gmail.com>
2022-12-14 14:42:44 -0800

    c function printing times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * prints out the times table upto 9
+  */
+void times_table(void)
+{
+	int numb, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
