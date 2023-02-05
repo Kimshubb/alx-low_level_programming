c502951810af6f1f90668262151d6f44d1b82554
Kimshubb <issakimanifmk@gmail.com>
2023-02-04 17:59:36 -0800

    more corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,6 +5,7 @@
   *@h pointer to the list_h to print
   *return the numbere of nodes printed
   */
+
 size_t print_list(const list_t *h);
 {
 	size_t count = 0;
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,10 +14,10 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
  	unsigned int len = 0;
 
-	while (strl[len])
+	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t))
+	new = malloc(sizeof(list_t));
 		if (!new)
 			return (NULL);
 	new->str = strdup(str);
