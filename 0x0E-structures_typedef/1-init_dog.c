39052fedecf99f8b84c23c034f45a49bf4ebaa86
Kimshubb <issakimanifmk@gmail.com>
2023-02-16 07:25:42 -0800

    gotcha

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 /**
   * init_dog - initializess a varible of type struct dog
@@ -10,16 +8,10 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
-int main()
-{
-	struct dog my_dog;
-	init_dog( &my_dog, "GHOST", 3.5, "Kim" );
-	printf("%s's dog is %s and is %f years old ", my_dog.owner ,my_dog.name, my_dog.age);
-	return 0;
+	if (d != NULL)
+	{
+		d ->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
