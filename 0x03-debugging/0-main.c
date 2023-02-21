9793f6589dadad2c6bf5fb8d2ba7f9cb8f749933
Kimshubb <issakimanifmk@gmail.com>
2023-02-20 21:05:10 -0800

    corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,14 +4,28 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
+void positive_or_negative(int n);
 int main(void)
 {
- int i;
-
- i = 98;
- positive_or_negative(i);
-
- return (0);
+	int test_case = 0;
+	printf("testing value:%d\n", test_case);
+	positive_or_negative(test_case);
+	return (0);
 }
+/** function implementation*/
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
 
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+}
