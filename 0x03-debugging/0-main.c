29443a9830ac7143e80e2211d04720ef3b26ccc2
Kimshubb <issakimanifmk@gmail.com>
2023-02-20 21:15:45 -0800

    more corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -7,25 +7,8 @@
 void positive_or_negative(int n);
 int main(void)
 {
-	int test_case = 0;
-	printf("testing value:%d\n", test_case);
-	positive_or_negative(test_case);
-	return (0);
-}
-/** function implementation*/
-void positive_or_negative(int n)
-{
-	if (n > 0)
-	{
-		printf("%d is positive\n",n);
-
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative", n);
-	}
+	int n = 0;
+	printf("testing value:%d\n", n);
+	positive_or_negative(n);
+	return (0)
 }
