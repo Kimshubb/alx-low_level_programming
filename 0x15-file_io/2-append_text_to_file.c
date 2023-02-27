8021043f1d15ce81e6c5a9d1b515100e022592e5
Kimshubb <issakimanifmk@gmail.com>
2023-02-27 13:25:54 -0800

    corrections

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,24 +3,29 @@
   * append text at the end of file
   * @filename: a pointer to the name of  a file
   * @textcontent thestring to add at the end of a file
-  *return -1 if functions fsils
+  * return -1 if functions fsils
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, bytes_written, text_len = 0;
 
 	if (filename == NULL)
 		return(-1);
-	if (filename != NULL)
+	if (text_content != NULL)
 	{
-		for(len=0; text_content[len];)
-			len++;
-		o = open(filename, O_WRONLY | O_APPEND);
-		w = write(o, text_content, len);
-
-		if (o == -1 || w ==-1)
-			return(-1);
-		close(o);
-		return(1);
+		while(text_content[text_len] != "\0")
+			text_len++;
 	}
+		fd = open(filename, O_WRONLY | O_APPEND);
+
+		if (fd == -1)
+			return(-1);
+		bytes_written = write(fd, text_content, text_len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return(-1);
+		}
+		close(fd);
+		return (1);
 }
