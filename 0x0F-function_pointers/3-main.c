6d4061186f593fc7e7a4382717f12cec7eb7838f
Kimshubb <issakimanifmk@gmail.com>
2023-02-27 12:13:18 -0800

    missing .h fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,16 +20,16 @@
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL || op[1]!= "\0")
+	if (get_op_func(op) == NULL || op[1] != "\0")
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if((*op == "/" && num2 == 0) || (*op == "%" && num2 == 0))
+	if((*op == "/" &&num2 == 0) || (*op == "%" &&num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d|n", get_op_func(op)(num1,num2));
+	printf("%d\n", get_op_func(op)(num1,num2));
 	return(0);
 }
