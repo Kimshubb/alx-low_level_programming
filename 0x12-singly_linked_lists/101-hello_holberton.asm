5a6970a96c345db30aeb6bf9e81bcea126cddc36
Kimshubb <issakimanifmk@gmail.com>
2023-03-03 11:13:26 -0800

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,16 @@
-     global    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+section .data
+fmt db	"%s", 10, 0
+msg db	"Hello, Holberton", 0
 
+section .text
+extern printf
+global main
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
