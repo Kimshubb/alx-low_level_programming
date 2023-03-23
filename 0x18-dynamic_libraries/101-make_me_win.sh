1d7968732b4052e9037736acc8756fc4338e08be
Kimshubb <issakimanifmk@gmail.com>
2023-03-23 13:06:32 -0700

    make me win

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_PRELOAD=https://github.com/Kimshubb/alx-low_level_programming/blob/b246cb70bb3521020cd6f16b9af4bc816c571cbc/0x18-dynamic_libraries/hacked.so
+https://github.com/holbertonschool/0x18.c/blob/7869708b060ff8a15c66e8ec6b063b108ed7e5d5/101-md5_gm 9 8 10 24 75 9
