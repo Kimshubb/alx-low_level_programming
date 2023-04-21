a1c3a44ec4a62ea031b0bbd0112fe43b4a79062e
Kimshubb <issakimanifmk@gmail.com>
2023-04-21 05:00:18 -0700

    Pop and return data in node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * pop_listint - deletes the head node of a linked list
  * @head: pointer to the first element in the linked list
@@ -9,16 +8,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t temp;
-	int n;
+	int value;
+	listint_t *temp;
 
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	temp = *head;
-	*head = *(head)->next;
-	n = temp->next;
+	value = temp->n;
+	*head = temp->next;
 	free(temp);
 
-	return (num);
+	return (value);
 }
