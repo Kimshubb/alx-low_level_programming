6c3faab90ab38a2d5a281dafc1406d058623daf7
Kimshubb <issakimanifmk@gmail.com>
2023-04-22 12:53:42 -0700

    this works

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,13 @@
+/**
+ * @delete_node at a given index
+ * @head pointer to a pointer to the head of a linked list
+ * @index :index of the node to be deleted starting at 0
+ * @Return 1 if successful -1 if not
+ */
+#include "lists.h"
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+{
+	listint_t *current, *prev;
+	unsigned int i;
+	if ()
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,12 +9,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 	listint_t *new_node, *temp;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
+	if (head == NULL || new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
@@ -23,17 +22,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 		return (*head);
 	}
 	temp = *head;
-	for (i = 0 ; i < idx - 1 && temp != NULL; i++)
+	for (i = 0 ; temp && i < idx; i++)
 	{
-		temp = temp->next;
+		if (i == idx -1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+		else
+			temp = temp->next;
 	}
-	if (temp == NULL)
-	{
-		free(new_node);
-		return (NULL);
-
-	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (*head);
+	return (NULL);
 }
