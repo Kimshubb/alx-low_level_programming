371be713b60d4e18bd41d6e39f77652f29c41262
Kimshubb <issakimanifmk@gmail.com>
2023-04-23 10:25:41 -0700

    frees a list with a loop,check floyds cycle detection algo

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - frees a listint_t linked list, safely
+ * @h: pointer to a pointer to the head of the list
+ * implements the floyd cycle detection algorithm
+ * Return: the size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+    listint_t *current, *next;
+    size_t count = 0;
+
+    if (h == NULL || *h == NULL)
+        return (0);
+
+    current = *h;
+    while (current)
+    {
+        count++;
+        next = current->next;
+        free(current);
+        if (next >= current)
+        {
+            *h = NULL;
+            exit(98);
+        }
+        current = next;
+    }
+
+    *h = NULL;
+    return (count);
+}
+
