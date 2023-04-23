91eeecddf3c425a2a965be60bbb1c57f48bdfa7c
Kimshubb <issakimanifmk@gmail.com>
2023-04-23 09:13:35 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,32 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
-
-dog_t *new_dog(char *name, float age, char *owner) 
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * @new_dog function creates a new dog
+ * @name dogs name
+ * @owner owners name
+ * @AGE dogs name
+ * @return new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j;
+	dog_t *new_dog;
 
-  	dog_t *new_dog = malloc(sizeof(dog_t));
-  	if (!new_dog)
-  	{ 
-		  return NULL;
-  	}
-  	new_dog->name = malloc(strlen(name) + 1);
-
-  	if (!new_dog->name)
-  	{
-  		  free(new_dog);
-    			return NULL;
-  	}
-
-  	strcpy(new_dog->name, name;
-  	new_dog->owner = malloc(strlen(owner) + 1);
-  	if (!new_dog->owner) 
-  	{
-  		free(new_dog->name);
-    	  	free(new_dog);
-    			return NULL;
-  	}
-  	strcpy(new_dog->owner, owner);  
+	for (i = 0 ; name[i]; i++);
+	for (j = 0 ; owner[j]; j++);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog = NULL)
+		return (NULL);
+	new_dog->name = malloc(i + 1);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
 	new_dog->age = age;
-  	return new_dog;
+	new_dog->owner = malloc(j + 1);
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (j = 0; owner[j]; j++)
+		new_dog->owner[j] = owner[j];
+	new_dog->owner[j] = '\0';
+	return (new_dog);
 }
