fc5d5627f6e3ee7814a2ccd6bf93a0ea56e4db3f
Kimshubb <issakimanifmk@gmail.com>
2023-06-24 13:38:26 -0700

    print name that works

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,7 @@
 {
 	int num1, num2;
 	char *op;
+
 	if(argc != 4)
 	{
 		printf("Error \n");
@@ -20,16 +21,18 @@
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	if (get_op_func(op) == NULL || op[1] != "\0")
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if((*op == "/" &&num2 == 0) || (*op == "%" &&num2 == 0))
+
+	if((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(num1,num2));
-	return(0);
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return 0;
 }
