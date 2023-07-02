5e4f24bfbfb4f42b36b647057ecdfcd7c4dfbac6
Kimshubb <issakimanifmk@gmail.com>
2023-07-02 12:38:26 -0700

    add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,65 +1,41 @@
 #include "lists.h"
-#include <string.h>
-#include <stdlib.h>
 
 /**
- * _strlen - Count the length of a string.
- * @s: String.
- * Return: Length.
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
-int _strlen(char *s)
-{
-	int c;/*Counter of characters*/
 
-	for (c = 0; s[c]; c++)
-		;
-
-	return (c);
-}
-
-/**
- * add_node_end - Add a new node at the end of a list_t list.
- * @head: Pointer to pointer head of the linked list.
- * @str: String of the node to add.
- * Return: The address of the new element.
- */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_end = NULL;/*New node at the end*/
-	list_t *end_node = NULL;/*Currently end node*/
+    list_t *new, *temp;
+    size_t nchar;
 
-	if (str == NULL)
-		return (NULL);
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
 
-	/*Allocate memory for a new node*/
-	new_end = malloc(sizeof(list_t));
-	if (new_end == NULL)
-		return (NULL);
+    new->str = strdup(str);
 
-	/*Create a new node*/
-	new_end->str = strdup(str);
+    for (nchar = 0; str[nchar]; nchar++)
+        ;
 
-	/*If strdup fail -> Free the new node*/
-	if (new_end->str == NULL)
-	{
-		free(new_end);
-		return (NULL);
-	}
+    new->len = nchar;
+    new->next = NULL;
+    temp = *head;
 
-	new_end->len = _strlen(new_end->str);
-	new_end->next = NULL;
+    if (temp == NULL)
+    {
+        *head = new;
+    }
+    else
+    {
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = new;
+    }
 
-	/*Found a current last node*/
-	if (*head == NULL)/*If the list are empty*/
-	{
-		*head = new_end;
-		return (new_end);
-	}
-
-	for (end_node = *head; end_node->next != NULL;)
-		end_node = end_node->next;
-
-	end_node->next = new_end;
-
-	return (new_end);
+    return (*head);
 }
