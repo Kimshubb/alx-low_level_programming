7399158244a4ca9bd387052cf9c3902601bdbd36
Kimshubb <issakimanifmk@gmail.com>
2023-08-03 12:33:05 -0700

    more bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,21 +6,20 @@
  * to get from one number to another.
  * @n: number 1;
  * @m: number 2;
- * Return: thenumber of buts needed to flip
+ * Return: the number of bits needed to flip
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int count;
-unsigned long int c_num;
-
-c_num = n ^ m;
-count = 0;
-while (c_num > 0)
-{
-if ((c_num & 1) != 0)
-count++;
-c_num = c_num >> 1;
-}
-return (count);
+	unsigned int bits;
+	unsigned long int f_num;
+      	f_num= n ^ m;
+	bits = 0;
+	while (c_num > 0)
+	{
+		if ((f_num & 1) != 0)
+		bits++;
+		f_num = f_num >> 1;
+	}
+	return (bits);
 }
