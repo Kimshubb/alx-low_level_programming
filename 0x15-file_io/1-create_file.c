a2259bef59b3703155b2a262a351a4988b1b05b0
Kimshubb <issakimanifmk@gmail.com>
2023-08-09 13:42:16 -0700

    permisision check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include"main.h"
 /**
-  * @filename - name of file to create
-  * @text_content - content of the filename
-  * @s - pointer ro astring
-  * return int
+  *create_file - creates a file 
+  * @filename: name of file to create
+  * @text_content: content of the filename
+  * return: int
   */
 int create_file(const char *filename, char *text_content)
 {
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 		return(-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		for (len = 0; text_content[len]);
 			len++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content,len);
 
-	if(fd == -1 || w ==-1)
+	if(fd == -1 || w == -1)
 		return(-1);
 	close(fd);
 	return(1);	
