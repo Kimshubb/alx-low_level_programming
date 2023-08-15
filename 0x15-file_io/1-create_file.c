1e2be5ee714eb59cca0201e339c7cecac721a625
Kimshubb <issakimanifmk@gmail.com>
2023-08-15 08:51:14 -0700

    create new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,50 @@
-#include"main.h"
+#include "main.h"
+
 /**
-  *create_file - creates a file 
-  * @filename: name of file to create
-  * @text_content: content of the filename
-  * return: int
-  */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
+* create_file - check the code for Holberton School students.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
-	int len = 0;
+	ssize_t nletters;
+	int file;
 
-	if (filename == NULL)
-		return(-1);
-	if (text_content != NULL)
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content,len);
-
-	if(fd == -1 || w == -1)
-		return(-1);
-	close(fd);
-	return(1);	
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
