27f8ea4ca1d18306d6402fe37f5dd19f647c4950
Kimshubb <issakimanifmk@gmail.com>
2023-08-18 12:09:43 -0700

    add node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,39 @@
+#include "lists.h"
+
 /**
- * @dlistint_len - returns the number of elements in a linked list
- * @h - pointer to the header
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
  */
-size_t dlistint_len(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t num = 0;
-	while (h != NULL)
+	dlistint_t *new;
+	dlistint_t *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
 	{
-		num++;
-		h = h->next;
+		while (h->prev != NULL)
+			h = h->prev;
 	}
-	return(num);
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+
+	return (new);
 }
 
